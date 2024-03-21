a0add1a7eefe82b242f9895f362cafb0fb318c82
Jorge Alexander Angulo <jalexander9609@gmail.com>
2024-03-20 20:59:01 -0500

    cambios en endPoint by id

--- src/controllers/restController.js
+++ src/controllers/restController.js
@@ -11,6 +11,7 @@ exports.getAllItemsTask = async (req, res) => {
     //console.log("getAllItemssss", req.query);
     const fileContent = fs.readFileSync(TasksFilePath, 'utf8');
     const items = JSON.parse(fileContent);
+    console.log(items);
     res.json(items);
   } catch (error) {
     res.status(500).json({ message: error.message });
@@ -22,20 +23,20 @@ exports.createTask = async (req, res) => {
   let tasks = [];
 
   try {
-    const { title, description } = req.body;
+    const { title, description, estado } = req.body;
     nuevaTask = {
       id: generateIdForTask(),
       title,
       description,
       fechaCreacion: new Date(),
-      estado: 'off'
+      estado: estado
     };
-    
+
     const fileContent = fs.readFileSync(TasksFilePath, 'utf8');
     tasks = JSON.parse(fileContent);
 
     if (!Array.isArray(tasks)) {
-        tasks = [];
+      tasks = [];
     }
 
     tasks.push(nuevaTask);
@@ -48,6 +49,51 @@ exports.createTask = async (req, res) => {
   }
 };
 
+
+exports.getTaskByDescripcion = async (req, res) => {
+
+  try {
+    const title = req.query.title;
+    let filter = []
+    const fileContent = fs.readFileSync(TasksFilePath, 'utf8');
+    const items = JSON.parse(fileContent);
+
+    // Filtrar los elementos que coincidan con el título
+    items.forEach(item => {
+      if (item['title'] == title) {
+        filter.push(item);
+      }
+    });
+
+    // Función para ajustar la fecha de cada objeto en el filtro
+    const ajustarFechas = (datos) => {
+      return datos.map(objeto => {
+        // Convertir la cadena de fecha en un objeto Date
+        const fechaActual = new Date(objeto.fechaCreacion);
+        // Restar 5 horas a la hora actual
+        fechaActual.setUTCHours(fechaActual.getUTCHours() - 5);
+        // Actualizar la propiedad "fechaCreacion" con la fecha ajustada
+        objeto.fechaCreacion = fechaActual;
+        return objeto;
+      });
+    };
+
+    // Ajustar las fechas en el filtro
+    filter = ajustarFechas(filter);
+
+    // Ordenar el filtro por la última fecha y hora
+    filter.sort((a, b) => {
+      return b.fechaCreacion - a.fechaCreacion;
+    });
+
+    res.json(filter.slice(0, 1));
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+
+
+};
+
 function generateIdForTask() {
-  return  Math.random().toString(36).substr(2, 9);
+  return Math.random().toString(36).substr(2, 9);
 }
--- src/routes/restRoutes.js
+++ src/routes/restRoutes.js
@@ -4,41 +4,127 @@ const router = express.Router();
 const restController = require('../controllers/restController');
 const  authMiddleware=require('../middleware/auth/authModdleware')
 
+
+
 /**
  * @openapi
  * /api/TasksCreate:
  *   post:
- *     description: Servicio obtener citas
+ *     summary: Crear una nueva tarea
+ *     description: Endpoint para crear una nueva tarea.
  *     tags:
- *       - Task
+ *       - Tarea
  *     parameters:
- *      - name: datos
- *        in: body
- *        schema:
- *          type: object
- *          required:
- *            - title
- *            - Description
- *          properties:
- *            title:
- *              type: string
- *            Description:
- *              type: string         
- *      - name: Authorization
- *        in: header
- *        description: Token de acceso
- *        required: true
- *        value: 7PAPusdFBo1l6brUQokAl2XjaaBdLtp6RxGZRuBZcEa9SGUuVEXj1YaYPehmGvio
- *        schema:
- *          type: string
+ *       - name: Authorization
+ *         in: header
+ *         description: Token de acceso
+ *         required: true
+ *         schema:
+ *           type: string
+ *           default: 7PAPusdFBo1l6brUQokAl2XjaaBdLtp6RxGZRuBZcEa9SGUuVEXj1YaYPehmGvio
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - title
+ *               - Description
+ *             properties:
+ *               title:
+ *                 type: string
+ *               Description:
+ *                 type: string
  *     responses:
  *       200:
- *         description: Ok
+ *         description: Tarea creada exitosamente
+ *       401:
+ *         description: No autorizado
+ *     x-code-samples:
+ *       - lang: curl
+ *         source: |
+ *           curl -X 'POST' \
+ *             'http://localhost:3000/api/TasksCreate' \
+ *             -H 'Authorization: 7PAPusdFBo1l6brUQokAl2XjaaBdLtp6RxGZRuBZcEa9SGUuVEXj1YaYPehmGvio' \
+ *             -H 'Content-Type: application/json' \
+ *             -d '{
+ *               "title": "Ejemplo de tarea",
+ *               "Description": "Descripción de la tarea"
+ *             }'
  */
-router.post('/TasksCreate', authMiddleware,restController.createTask);
+router.post('/TasksCreate', authMiddleware, restController.createTask);
+/**
+ * @openapi
+ * /api/getTasksAll:
+ *   get:
+ *     summary: Obtener todas las tareas
+ *     description: Endpoint para obtener todas las tareas.
+ *     tags:
+ *       - Tarea
+ *     parameters:
+ *       - name: Authorization
+ *         in: header
+ *         description: Token de acceso
+ *         required: true
+ *         schema:
+ *           type: string
+ *           default: 7PAPusdFBo1l6brUQokAl2XjaaBdLtp6RxGZRuBZcEa9SGUuVEXj1YaYPehmGvio
+ *     responses:
+ *       200:
+ *         description: Lista de tareas obtenida exitosamente
+ *       401:
+ *         description: No autorizado
+ *     x-code-samples:
+ *       - lang: curl
+ *         source: |
+ *           curl -X 'GET' \
+ *             'http://localhost:3000/api/getTasksAll' \
+ *             -H 'Authorization: 7PAPusdFBo1l6brUQokAl2XjaaBdLtp6RxGZRuBZcEa9SGUuVEXj1YaYPehmGvio' \
+ *             -H 'accept: application/json'
+ */
+
+
+router.get('/getTasksAll', authMiddleware, restController.getAllItemsTask);
+
+/**
+ * @openapi
+ * /api/getTasksbyid:
+ *   get:
+ *     summary: Obtener tarea por ID
+ *     description: Endpoint para obtener una tarea por su ID.
+ *     tags:
+ *       - Tarea
+ *     parameters:
+ *       - name: Authorization
+ *         in: header
+ *         description: Token de acceso
+ *         required: true
+ *         schema:
+ *           type: string
+ *           default: "7PAPusdFBo1l6brUQokAl2XjaaBdLtp6RxGZRuBZcEa9SGUuVEXj1YaYPehmGvio"
+ *       - name: descripcion
+ *         in: query
+ *         description: Descripción de la tarea
+ *         required: true
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Tarea encontrada exitosamente
+ *       401:
+ *         description: No autorizado
+ *     x-code-samples:
+ *       - lang: curl
+ *         source: |
+ *           curl -X 'GET' \
+ *             'http://localhost:3000/api/getTasksbyid?descripcion=descripcion_de_ejemplo' \
+ *             -H 'Authorization: Bearer 7PAPusdFBo1l6brUQokAl2XjaaBdLtp6RxGZRuBZcEa9SGUuVEXj1YaYPehmGvio' \
+ *             -H 'Accept: application/json'
+ */
+router.get('/getTasksbyid', authMiddleware, restController.getTaskByDescripcion);
 
 
 
-router.get('/getTasksAll',authMiddleware,restController.getAllItemsTask)
 
 module.exports = router;
